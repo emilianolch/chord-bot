7f28905a0e13b2f4975f7f5724f625a9b6ad20c5
Emiliano Lopez <emilianolch@gmail.com>
2021-10-11 15:00:47 -0300

    searching for a song

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-require('dotenv').config();
+require('dotenv').config()
+const axios = require('axios')
+const TelegramBot = require('node-telegram-bot-api')
 
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
 
 // Start message
 bot.onText(/^\/start$/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Hola!");
+  const name = msg.from.first_name
+  const text = `Hola ${name}!\n¿Qué canción estás buscando?`
+  bot.sendMessage(msg.chat.id, text)
+})
+
+// Search for a song
+bot.onText(/^[^\/].*/, (msg) => {
+  const baseUrl = "https://acordes.lacuerda.net/busca.php?canc=0&exp="
+  const searchUrl = baseUrl + encodeURIComponent(msg.text)
+
+  axios.get(searchUrl).then(response => {
+    console.log(response.data)
+    bot.sendMessage(msg.chat.id, msg.text)
+  })
 })
